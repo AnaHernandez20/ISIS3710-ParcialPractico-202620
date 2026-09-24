ec743146a6c42364765507c659bf030fbe0fc8d6
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2026-09-24 09:24:29 -0500

    a ver si alcanzo cypress

---
+++ cypress/e2e/planes/crearPlan.cy.ts
@@ -0,0 +1,10 @@
+//Prueba exitosa y fallida de crear 1 plan
+
+describe('Crear Plan', () => {
+  it('Debería crear un plan exitosamente', () => {
+    
+  });
+
+  it('Debería fallar al crear un plan porque los datos están mal', () => {
+  });
+});
